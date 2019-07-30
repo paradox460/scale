c58030def9510bb7d9b6b07a59610137b482c32a
Jeff Sandberg <paradox460@gmail.com>
2019-07-29 17:08:59 -0700

    Add circuits_uart

--- mix.exs
+++ mix.exs
@@ -21,6 +21,7 @@ defmodule Scale.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:circuits_uart, "~> 1.3"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
